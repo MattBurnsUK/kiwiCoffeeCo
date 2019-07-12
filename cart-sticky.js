2e85ca497aac837d061ee5b8fcc947b45f3888b4
MattBurnsUk <hello@matt-burns.com>
2019-07-11 19:51:26 -0700

    fixed bug on mobile add to cart form

--- cart-sticky.js
+++ cart-sticky.js
@@ -2,6 +2,20 @@ var button = document.getElementById("cart-sticky");
 var popupButton = document.getElementById('cart-sticky-popup');
 popupButton.addEventListener("click", cartStickyClick);
 
+/*
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var staticCartParent = document.getElementById('static-cart-container');
+var staticCart = document.getElementById('addToCart');
+
+function removeStaticCart(){
+    if (w < 425) {
+        staticCartParent.removeChild(staticCart);
+    }
+}
+
+removeStaticCart();
+
+*/
 
 function cartStickyClick(e){
     var stateWhenClicked = button.getAttribute("class");
